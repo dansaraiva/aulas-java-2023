a21b3a61047b2d1c4ef9f6d70756e6653ed3370a
Daniel Saraiva <77885686+dansaraiva@users.noreply.github.com>
2023-08-28 22:20:25 -0300

    Variaveis

---
+++ Main.java
@@ -0,0 +1,18 @@
+package variaveis;
+
+public class Main {
+    public static void main(String[] args) {
+        int idade;
+        idade = 10;
+        double peso;
+        peso = 42.0;
+        double altura;
+        altura = 1.40;
+        String nome;
+        nome = "Ana Silva";
+        System.out.println("Bem vindo(a), "+nome+".");
+        System.out.println("Sua idade é: "+ idade + " anos.");
+        System.out.println("Seu peso é: "+ peso + " kg.");
+        System.out.println("Sua altura é: "+ altura + " m.");
+    }
+}
